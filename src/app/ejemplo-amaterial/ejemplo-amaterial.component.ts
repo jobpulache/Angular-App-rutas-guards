2f52adb8e1796b83bb3bd51a9860651cfe4ec820
Job <pulachecarrenojob837@gmail.com>
2024-11-22 13:17:42 -0500

    initial commit

---
+++ src/app/ejemplo-amaterial/ejemplo-amaterial.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ejemplo-amaterial',
+  standalone: true,
+  imports: [],
+  templateUrl: './ejemplo-amaterial.component.html',
+  styleUrl: './ejemplo-amaterial.component.css'
+})
+export class EjemploAmaterialComponent {
+
+}
